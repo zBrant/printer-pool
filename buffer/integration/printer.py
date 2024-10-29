4e7330600c869c07ea4c68087754917808cdb4a7
EduardoBrant <carlos23brant@hotmail.com>
2024-10-28 21:20:27 -0400

    removing the buffer

--- buffer/integration/printer.py
+++
@@ -1,7 +0,0 @@
-import json
-
-class Printer:
-
-    @staticmethod
-    def send_message(message: str, printer_socket) -> None:
-        printer_socket.send((json.dumps(message) + '\n').encode('utf-8')[:1024])
